6e287697e3f0cd18fd785e31caf9e02a1a2c33da
Casa\HB <handersonsylva@gmail.com>
2017-03-13 20:01:08 -0300

    Final

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 /*importar as configurações do servidor*/
 var app = require('./config/server');
 
-/*parametrizar a porta de escuta*/
-var server = app.listen(8080,function(){
+/*parametrizar a porta de escuta
+Caso tenha problemas em usar a porta 80 no windows olhar esse tutorial
+http://marcos-tijucano.blogspot.com.br/2012/08/problema-para-iniciar-apache-na-porta-80.html*/
+
+var server = app.listen(80,function(){
     console.log("Servidor ON");
 })
 
